6ddc18399db637c7aec966a44996f6c9d8b1ca22
Lucas Diniz <lucas.mdiniz@live.com>
2020-08-09 10:00:55 -0300

    get backend ready to deploy on heroku

--- packages/backend/src/index.js
+++ packages/backend/src/index.js
@@ -5,18 +5,18 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 5000;
 const transactionRouter = require('./routes/transaction');
 const budgetRouter = require('./routes/budget');
 const weeklyRouter = require('./routes/weekly');
 const userRouter = require('./routes/user');
 
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(transactionRouter);
 app.use(budgetRouter);
 app.use(weeklyRouter);
 app.use(userRouter);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port);
--- packages/web/src/services/api.jsx
+++ packages/web/src/services/api.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/',
+  withCredentials: true,
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
